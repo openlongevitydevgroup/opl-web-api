855c7142ef0304d9d89e3257eb007cf44046acd0
angelotalay <angelogabrieltalay@gmail.com>
2023-06-13 10:42:53 +0100

    Connection to google cloud

--- OPL/settings.py
+++ OPL/settings.py
@@ -84,15 +84,13 @@ WSGI_APPLICATION = "OPL.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": "5432",
+        "NAME": 'OPL',
+        "USER": 'angelo',
+        "PASSWORD": 'Brause134!',
+        "HOST": '34.89.3.62',
     }
 }
 
