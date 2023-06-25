bb80e4b752b53ff3fd1cae7531556530f2caea17
angelotalay <angelogabrieltalay@gmail.com>
2023-06-25 21:30:00 +0100

    Changed settings.

--- OPL/settings.py
+++ OPL/settings.py
@@ -57,9 +57,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-# CORS_ALLOWED_ORIGINS = ["https://localhost",
-#                          os.environ.get("HOME_IP")]#Home address
-CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOWED_ORIGINS = ["localhost",os.environ.get("HOME_IP")]#Home address
+# CORS_ALLOW_ALL_ORIGINS = True 
 REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"]}
 
 ROOT_URLCONF = "OPL.urls"
