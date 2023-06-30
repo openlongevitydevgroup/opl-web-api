7d628b604070c9679a42fb0cf4f01091ffff095d
angelotalay <angelogabrieltalay@gmail.com>
2023-06-30 17:46:20 +0100

    Added domain to trusted origins.

--- OPL/settings.py
+++ OPL/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#Load environment variables
+# Load environment variables
 load_dotenv()
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOME_IP'), 'localhost', "127.0.0.1", "admin.longevityknowledge.app"] #Server IP - set to env variable
+ALLOWED_HOSTS = [os.environ.get('HOME_IP'), 'localhost', "127.0.0.1",
+                 "admin.longevityknowledge.app"]  # Server IP - set to env variable
 # BIND = ['0.0.0.0']
 
 # Application definition
@@ -57,9 +58,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",f'http://{os.environ.get("HOME_IP")}','http://127.0.0.1:8000', 'https://www.longevityknowledge.app', 'https://admin.longevityknowledge.app']#Home address
-# CORS_ALLOW_ALL_ORIGINS = True 
-REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"]}
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", f'http://{os.environ.get("HOME_IP")}', 'http://127.0.0.1:8000',
+                        'https://www.longevityknowledge.app', 'https://admin.longevityknowledge.app']  # Home address
+CSFRF_TRUSTED_ORIGINS = ["https://admin.longevityknowledge.app"]
+REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": [
+    "rest_framework.permissions.AllowAny"]}
 
 ROOT_URLCONF = "OPL.urls"
 
@@ -90,7 +93,7 @@ DATABASES = {
         "NAME": os.environ.get('DB_NAME'),
         "USER": os.environ.get('POSTGRES_USER'),
         "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'), 
+        "HOST": os.environ.get('DB_HOST'),
         "PORT": "5432"
     }
 }
