0b7354f347bfa9a83b4e14486d3d0932b7752bde
angelotalay <angelogabrieltalay@gmail.com>
2023-07-10 09:51:12 +0100

    Cookie domain settings.

--- OPL/settings.py
+++ OPL/settings.py
@@ -155,6 +155,7 @@ csrf_cookie_secure = eval(configuration['settings']['csrf_cookie_secure'])
 SESSION_COOKIE_SECURE = session_cookie_secure
 CSRF_COOKIE_SECURE = csrf_cookie_secure
 
-SESSION_COOKIE_DOMAIN = os.environ.get("SESSION_COOKIE_DOMAIN")
+session_cookie_domain = configuration['settings']['session_cookie_domain']
+SESSION_COOKIE_DOMAIN = session_cookie_domain
 
 CSRF_TRUSTED_ORIGINS=['https://admin.longevityknowledge.app', f"http://{os.environ.get('HOME_IP')}", 'http://localhost', "http://127.0.0.1"]
