a15971c827df78d417961ea57e0a3b08116eb496
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-11 15:28:00 +0100

    Added model for submissions of comments on open-problems.

--- open_problems/models/submissions.py
+++ open_problems/models/submissions.py
@@ -1,28 +1,42 @@
-from django.db import models 
-from .contacts_users import Contact 
+from django.db import models
+from .contacts_users import Contact
+from .references import Reference
+from .open_problems import OpenProblems
+
+
 class Submission(models.Model):
-    submission_id = models.IntegerField() 
-    title = models.CharField(max_length=300, null=True)
+    submission_id = models.AutoField(primary_key=True)
+    date = models.DateField(auto_now=False, auto_now_add=False)
     full_text = models.TextField(null=True)
-    # Are contacts required to submit ?? 
+    question = models.ForeignKey(OpenProblems, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    # Are contacts required to submit ??
+    class Meta:
+        abstract = True
+
+
+class ResearchSubmission(Submission):
+    parent = models.ForeignKey("self", null=True, on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
-        return f"{self.submission_id}: {self.title}"
-    class Meta:
-        abstract = True 
+        return f"{self.submission_id}: {self.question}"
 
-class ResearchSubmission(Submission): 
-    ...
 
-class SolutionSubmission(Submission): 
-    ... 
+class SolutionSubmission(Submission):
+    parent = models.ForeignKey("self", null=True, on_delete=models.DO_NOTHING)
 
-class ResearchThread(models.Model): #Non cyclic table for now. 
+    def __str__(self) -> str:
+        return f"{self.submission_id}: {self.question}"
+
+
+class ResearchThread(models.Model):  # Non cyclic table for now.
     primary_submission = models.OneToOneField(ResearchSubmission)
     response_submission = models.ForeignKey(ResearchSubmission)
 
-    def __str__(self) -> str: 
+    def __str__(self) -> str:
         return f"{self.primary_submission.submission_id} :  {self.response_submission.submission_id}"
+
 
 class SolutionThread(models.Model):
     primary_submission = models.OneToOneField(SolutionSubmission)
