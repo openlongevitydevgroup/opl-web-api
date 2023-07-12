1906c0687d9d56c1bcadbf68e266086df3c8e328
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-12 12:46:19 +0100

    Added url paths for api views.

--- open_problems/urls.py
+++ open_problems/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path 
 from open_problems import views
-from open_problems.admin import OPAdmin
 
 urlpatterns = [
-    path('open-problems/', views.questions_list), #All questions
-    path('open-problems/root', views.questions_root),
-    path('open-problems/<int:id>', views.question_detail), 
-    path('open-problems/submit', views.submitted_questions),
+    path('', views.questions_list), #All questions
+    path('root', views.questions_root),
+    path('<int:id>', views.question_detail), 
+    path('submit', views.submitted_questions),
     path('verify-token', views.verify_token)
 ]
