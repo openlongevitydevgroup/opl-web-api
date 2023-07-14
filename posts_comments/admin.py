6eb2d873cd11f3e2114193df774d8bbde23c8ff7
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-14 16:04:22 +0100

    Made user submitted texts read only.

--- posts_comments/admin.py
+++ posts_comments/admin.py
@@ -3,6 +3,9 @@ from .models.submissions import *
 from .models.comments import *
 # Register your models here.
 
+class SubmissionAdmin(admin.ModelAdmin): 
+    readonly_fields = ["full_text"]
 
-admin.site.register(Submission)
+admin.site.register(Submission,SubmissionAdmin)
 admin.site.register(SubmittedReferences)
+
