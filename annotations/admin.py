a658c03d6829dab03c7855f10c394003b1108e42
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-16 22:20:23 +0100

    Finished basic annotations for posts.

--- annotations/admin.py
+++ annotations/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from .models.annotations import AnnotationsProblems
 from .models.theory import Theory, TheoryProblem 
 from .models.species import Species
+from .models.genes import Gene, GeneProblem
 # Register your models here.
+
+# class AnnotationsProblemsAdmin(admin.ModelAdmin): 
+#     list_display = [field.name for field in AnnotationsProblems._meta.fields]
+    
+admin.site.register(Theory)
+admin.site.register(TheoryProblem)
+admin.site.register(Species)
+admin.site.register(Gene)
+admin.site.register(GeneProblem)
