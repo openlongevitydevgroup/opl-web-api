84d09e439a9cd808310f674c5d58413187954baa
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-17 18:39:24 +0100

    na

--- open_problems/models/contacts_users.py
+++ open_problems/models/contacts_users.py
@@ -16,6 +16,7 @@ class JobField(JobInformation):
         return f'{self.info_title}'
 
 class Contact(models.Model): 
+    contact_id = models.AutoField(primary_key=True,null=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
