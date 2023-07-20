2255c67376173ba4864bb924ac0c93c87b44ee2f
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-20 09:54:11 +0100

    Add model and serializer for comments.

--- posts_comments/admin.py
+++ posts_comments/admin.py
@@ -3,9 +3,9 @@ from .models.submissions import *
 from .models.comments import *
 # Register your models here.
 
-class SubmissionAdmin(admin.ModelAdmin): 
+class CommentSubmissionAdmin(admin.ModelAdmin): 
     readonly_fields = ["full_text"]
 
-admin.site.register(Submission,SubmissionAdmin)
-admin.site.register(SubmittedReferences)
+admin.site.register(Submission,CommentSubmissionAdmin)
+admin.site.register(SubmittedReferences, CommentSubmissionAdmin)
 
--- posts_comments/models/comments.py
+++ posts_comments/models/comments.py
@@ -1,11 +1,12 @@
 from django.db import models
 from open_problems.models.open_problems import OpenProblems
+from posts_comments.models.submissions import Submission
 
 class Comments(models.Model):
     comment_id = models.AutoField(primary_key=True) 
-    question = models.ForeignKey(OpenProblems, null=False, blank=False, on_delete=models.CASCADE)
+    submission = models.ForeignKey(Submission, on_delete=models.DO_NOTHING)
     parent = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
     full_text = models.TextField(blank=False, null=False)
 
     def __str__(self) -> str:
-        return f"{self.comment_id}:{self.parent}"
+        return f"{self.comment_id}:{self.full_text}"
