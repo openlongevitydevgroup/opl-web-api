a1694703dd7274140077e35d20575343c488cb2b
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-20 15:51:39 +0100

    Created utils folder for general functions. Created a recursive serializer for hierarchical models.

---
+++ utils/recursive_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+# Serializer to retrieve children of parent node
+class RecursiveSerializer(serializers.Serializer):
+    ''' Handle self-nested serializer'''
+    def to_representation(self, instance):
+        serializer = self.parent.parent.__class__(
+            instance, context=self.context)
+        return serializer.data
+
