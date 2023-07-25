f86a60b7cfbcf02ceed68a708dba5ab6620d7b0c
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-25 15:38:08 +0100

    Created a simple view for filtering open problems query set by any annotation.

---
+++ annotations/views/filter_annotations.py
@@ -0,0 +1,16 @@
+from open_problems.models.open_problems import OpenProblems
+from annotations.models.theory import Theory
+def filter_by_annotations(annotations):
+    query_set = OpenProblems.objects.all() 
+    for annotation_name, annotation_value in annotations.items():
+        try:
+            # Retrieve the model dynamically based on the annotation name
+            annotation_model = globals()[annotation_name]
+            # Check if the model name is the same as the annotation name
+            if annotation_model.__name__ == annotation_name:
+                # Use the correct related name for the filtering (assuming 'theoryproblem__theory')
+                related_name = f"{annotation_name.lower()}problem__{annotation_name.lower()}"
+                query_set = query_set.filter(**{f"{related_name}__pk": annotation_value})
+        except KeyError:
+            pass
+    return query_set
---
+++ annotations/views/filter_annotations_view.py
@@ -0,0 +1,20 @@
+from .filter_annotations import filter_by_annotations
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from open_problems.serializers import OPSerializer
+
+@api_view(["GET"])
+def filter_annotations(request):
+    if request.method == "GET":
+        annotations = request.GET
+        try:
+            open_problems_filtered = filter_by_annotations(annotations)
+            serializer = OPSerializer(open_problems_filtered, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception: 
+            return Response({'error': str(Exception)}, status=status.HTTP_400_BAD_REQUEST)
+        
+            
+
+    
