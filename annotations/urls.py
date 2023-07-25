ac50b6ab9cab3a693d231e58cea35f266a2887b7
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-25 15:38:59 +0100

    Added annotation filter view to url.

--- annotations/urls.py
+++ annotations/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path 
 from annotations.views.theory_view import get_theories
 from annotations.views.gene_view import get_genes
+from annotations.views.filter_annotations_view import filter_annotations
 
+
+# Base url api/annotations/filter
 urlpatterns = [
     path("<int:id>/theory", get_theories), 
-    path("<int:id>/genes", get_genes)
+    path("<int:id>/genes", get_genes),
+    path("filter/", filter_annotations)
 ]
+
