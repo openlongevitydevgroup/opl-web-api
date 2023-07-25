98620b289f7b86b1de587b79e1077973fa3df050
Angelo Talay <angelogabrieltalay@gmail.com>
2023-07-25 15:39:13 +0100

    na

--- open_problems/serializers.py
+++ open_problems/serializers.py
@@ -3,13 +3,7 @@ from .models.open_problems import OpenProblems, RelatedProblem, SubmittedProblem
 from .models.references import Reference
 from utils.recursive_serializer import RecursiveSerializer
 
-# Serializer to retrieve children of parent node
-# class RecursiveSerializer(serializers.Serializer):
-#     ''' Handle self-nested serializer'''
-#     def to_representation(self, instance):
-#         serializer = self.parent.parent.__class__(
-#             instance, context=self.context)
-#         return serializer.data
+
 # Serializer for parent node of open problem
 class ParentSerializer(serializers.ModelSerializer): 
     class Meta: 
