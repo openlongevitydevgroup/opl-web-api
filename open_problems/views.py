e11473f52f41c770c6c5f8352d716147c8fccd3d
Angelo Talay <angelogabrieltalay@gmail.com>
2023-08-02 11:00:49 +0100

    Added is_active filtering to views.

--- open_problems/views.py
+++ open_problems/views.py
@@ -3,11 +3,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from open_problems.serializers import OPSerializer
-from .models.open_problems import OpenProblems
-from .models.open_problems import ProblemReference
+from models.open_problems import OpenProblems
+from models.open_problems import ProblemReference
 from open_problems.serializers import SubmittedProblemSerializer as SPSerializer
 from open_problems.serializers import ParentSerializer as PSerializer
-from .models.open_problems import SubmittedProblems as SOP
+from models.open_problems import SubmittedProblems as SOP
 from open_problems.serializers import ContactSerializer
 from open_problems.serializers import FilterReferenceSerializer
 from requests import post
@@ -15,25 +15,27 @@ from requests import post
 
 @api_view(['GET'])
 def questions_list(request):
-    questions = OpenProblems.objects.all().order_by("problem_id")
+    questions = OpenProblems.objects.filter(is_active=True).order_by("problem_id")
     serializer = OPSerializer(questions, many=True)
     return Response(serializer.data)
+
 
 # Get the root questions
 
 
 @api_view(['GET'])
 def questions_root(request):
-    root_questions = OpenProblems.objects.filter(parent_problem=None).order_by("problem_id")
+    root_questions = OpenProblems.objects.filter(parent_problem=None, is_active=True).order_by("problem_id")
     serializer = OPSerializer(root_questions, many=True)
     return Response(serializer.data)
+
 
 # Returns a single question with its child questions
 
 
 @api_view(['GET'])
 def question_detail(request, id):
-    ''' Retrieve a single question and its children'''
+    """ Retrieve a single question and its children"""
     problem = OpenProblems.objects.get(problem_id=id)
     parent = problem.parent_problem
     serializer = OPSerializer(problem)
@@ -43,21 +45,22 @@ def question_detail(request, id):
         parent_serializer = None
     else:
         parent_serializer = PSerializer(parent).data
-    if not contact: 
-        contact_serializer = None 
-    else: 
+    if not contact:
+        contact_serializer = None
+    else:
         contact_serializer = ContactSerializer(contact).data
 
     data = {
-        "open_problem": serializer.data, 
-        "parent_data": parent_serializer, 
+        "open_problem": serializer.data,
+        "parent_data": parent_serializer,
         "contact": contact_serializer
     }
     return Response(data)
 
+
 @api_view(['GET', 'POST'])
 def submitted_questions(request):
-    ''' Submit a new question '''
+    """ Submit a new question """
     if request.method == 'GET':
         problems = SOP.objects.all()
         serializer = SPSerializer(problems, many=True)
@@ -68,21 +71,24 @@ def submitted_questions(request):
             problem_serializer.save()
             return Response(problem_serializer.data, status=status.HTTP_201_CREATED)
 
+
 @api_view(['POST'])
 def verify_token(request):
     ''' Verify google recaptcha token'''
     if request.method == 'POST':
         data = request.data
-        post_request = post('https://www.google.com/recaptcha/api/siteverify', data={'secret':data['secret'], 'response':data['response'] })
+        post_request = post('https://www.google.com/recaptcha/api/siteverify',
+                            data={'secret': data['secret'], 'response': data['response']})
         content = post_request.text
         return Response(content)
 
+
 # Get references for an open problem
 @api_view(['GET'])
-def get_references(request,id): 
+def get_references(request, id):
     references = ProblemReference.objects.filter(problem_id=id)
     serializer = FilterReferenceSerializer(references, many=True)
-    if not references: 
-        return Response(data=serializer.data,status=status.HTTP_204_NO_CONTENT)
-    else: 
-        return Response(data=serializer.data,status=status.HTTP_202_ACCEPTED)
+    if not references:
+        return Response(data=serializer.data, status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
