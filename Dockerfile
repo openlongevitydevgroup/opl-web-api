0bccf81470832f95a95711a9205df2be47fd3db2
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-08-17 18:57:40 +0100

    Bug fixes with retrieving harvard information from cross-ref api.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ ENV PYTHONUNBUFFERED 1
 RUN mkdir /backend
 WORKDIR /backend
 ADD . /backend/
+RUN pip install
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
--- utils/get_doi_information.py
+++ utils/get_doi_information.py
@@ -2,21 +2,24 @@ from crossref.restful import Works
 
 
 def format_harvard(data):
-    """Gets reference information from cross-ref api response and creates citation """
-    authors = data['author']
-    year = data['published-print']['date-parts'][0][0]
-    title = data['title'][0]
-    journal = data['short-container-title'][0]
-    volume = data['volume']
-    issue = data['issue']
-    pages = data['page']
-    publisher = data['publisher']
-    doi = data['DOI']
+    """Gets reference information from cross-ref api response and creates citation"""
+    authors = data.get('author', [])
+
+    date_parts = data.get('published-print', {}).get('date-parts', [])
+    year = date_parts[0][0] if date_parts and date_parts[0] else ''
+
+    title = data.get('title', [''])[0]
+    journal = data.get('short-container-title', [''])[0]
+    volume = data.get('volume', '')
+    issue = data.get('issue', '')
+    pages = data.get('page', '')
+    publisher = data.get('publisher', '')
+    doi = data.get('DOI', '')
 
     author_list = ', '.join([f"{author['family']}, {author['given'][0]}." for author in authors])
 
     citation = f"{author_list} ({year}) \"{title}.\" *{journal}*, {volume}({issue}), {pages}. {publisher}. doi:{doi}"
-    return {'title': title, 'year': year, 'journal': journal, 'volume': volume, 'citation': citation, "doi": {doi}}
+    return {'title': title, 'year': year, 'journal': journal, 'volume': volume, 'citation': citation, 'doi': doi}
 
 
 def doi_crossref_search(doi: str):
