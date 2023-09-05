5a1a053bc8de349baa2bfc5a73bb2fe7144f545c
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-09-05 16:28:54 +0100

    Added correct origin

--- OPL/settings.py
+++ OPL/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -63,7 +62,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://longevityknowledge.com",
+    "https://longevityknowledge.app",
     "http://127.0.0.1",
 ]
 
