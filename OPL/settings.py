6f498304863ab83039dee6da541c3dfa4bc66177
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2023-09-25 13:26:13 +0100

    fixed static url

--- OPL/settings.py
+++ OPL/settings.py
@@ -123,8 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = "static/"
-STATIC_ROOT = str(cwd_path.joinpath("static"))
+STATIC_URL = "/static/"
+STATIC_ROOT = str(cwd_path.joinpath("staticfiles"))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
